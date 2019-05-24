a9598d56b3166e8fe27066f0831974ecece268ef
Alex <alex.A4.25@yandex.ru>
2019-05-24 22:22:03 +0300

    Delete methods to get friends/photos response

--- Android/app/src/main/java/alexa4/friendphoto/ui/ActivityCallback.java
+++ Android/app/src/main/java/alexa4/friendphoto/ui/ActivityCallback.java
@@ -9,17 +9,5 @@ public interface ActivityCallback {
      * If user authenticated, then hide webView and start to loading friends
      * else show starting page
      */
-    void userAuthenticated(boolean isAuth);
-
-    /**
-     * If friends downloaded then send true and show them, else show some error
-     * @param isDownloaded
-     */
-    void friendsDownloaded(boolean isDownloaded);
-
-    /**
-     * If photos downloaded, then send true and show them, else show some error
-     * @param isDownloaded
-     */
-    void photosDownloaded(boolean isDownloaded);
+    void authenticateUser(boolean isAuth);
 }
