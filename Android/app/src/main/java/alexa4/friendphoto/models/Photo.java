134202f64bd4ba73691d131ab5283c888e3e1176
Alex <alex.A4.25@yandex.ru>
2019-05-24 22:15:48 +0300

    Added text to phot

--- Android/app/src/main/java/alexa4/friendphoto/models/Photo.java
+++ Android/app/src/main/java/alexa4/friendphoto/models/Photo.java
@@ -12,10 +12,15 @@ public class Photo {
     // Different sizes of image to show in different places
     public final String mBigSize;
     public final String mSmallSize;
+    // Text of photo
+    public final String mText;
 
     public Photo(JSONObject json) throws JSONException {
+        mText = json.getString("text");
+
         JSONArray array = json.getJSONArray("sizes");
         mBigSize = array.getJSONObject(4).getString("url");
         mSmallSize = array.getJSONObject(0).getString("url");
+
     }
 }
