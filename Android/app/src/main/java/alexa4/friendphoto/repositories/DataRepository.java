3555fde833332589f897c0106b7060335d6ebf83
Alex <alex.A4.25@yandex.ru>
2019-05-24 22:20:22 +0300

    Fully changed logic of authentication and data downloading

--- Android/app/src/main/java/alexa4/friendphoto/repositories/DataRepository.java
+++ Android/app/src/main/java/alexa4/friendphoto/repositories/DataRepository.java
@@ -1,16 +1,19 @@
 package alexa4.friendphoto.repositories;
 
+import android.content.SharedPreferences;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 import alexa4.friendphoto.controllers.FriendsController;
 import alexa4.friendphoto.controllers.PhotosController;
+import alexa4.friendphoto.models.Friend;
+import alexa4.friendphoto.models.Photo;
 import alexa4.friendphoto.models.User;
 import alexa4.friendphoto.ui.ActivityCallback;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.schedulers.Schedulers;
-import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -28,7 +31,8 @@ public class DataRepository implements AuthCallback {
     private final FriendsController mFriends = new FriendsController();
     private final PhotosController mPhotos = new PhotosController();
     private User mUser;
-    private CompositeDisposable mDisposable = new CompositeDisposable();
+
+    private StorageProvider mStorage = new StorageProvider();
 
     // The link to UI
     private ActivityCallback mUI;
@@ -36,63 +40,52 @@ public class DataRepository implements AuthCallback {
     public DataRepository(ActivityCallback UI) {
         mUI = UI;
     }
-    //    public static final MediaType JSON
-//            = MediaType.get("application/json; charset=utf-8");
-
 
     /**
      * RX call of downloading photos of specified friend
      *
      * @param friendId id of friend whose photos need to download
      */
-    public void getPhotosObservable(int friendId) {
-        mDisposable.add(Observable.<Boolean>create(emitter -> {
-                    Request request = new Request.Builder()
-                            .url("https://api.vk.com/method/photos.get?owner_id="
-                                    + friendId + "&album_id=wall&v=5.95&access_token="
-                                    + mUser.mToken)
-                            .build();
-                    try (Response response = client.newCall(request).execute()) {
-                        Log.d(TAG, response.body().toString());
-
-                        if (mPhotos.isIdenticalId(friendId))
-                            emitter.onNext(false);
-                        else {
-                            mPhotos.setOwnerId(friendId);
-                            mPhotos.setPhotos(response.body().string());
-                            emitter.onNext(true);
-                        }
-                    }
-                    emitter.onComplete();
-                })
-                        .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe(mUI::photosDownloaded,
-                                e -> mUI.photosDownloaded(false))
-        );
+    public Observable<ArrayList<Photo>> downloadPhotos(int friendId) {
+        return Observable.<ArrayList<Photo>>create(emitter -> {
+            Request request = new Request.Builder()
+                    .url("https://api.vk.com/method/photos.get?owner_id="
+                            + friendId + "&album_id=wall&v=5.95&access_token="
+                            + mUser.mToken)
+                    .build();
+            try (Response response = client.newCall(request).execute()) {
+                if (!mPhotos.isIdenticalId(friendId) && response.isSuccessful()) {
+                    mPhotos.setOwnerId(friendId);
+                    mPhotos.setPhotos(response.body().string());
+                }
+                emitter.onNext(mPhotos.mPhotos);
+            }
+            emitter.onComplete();
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread()
+                );
     }
 
     /**
-     * Download information about friends
+     * Return observable which should download information about friends
      */
-    public void downloadFriends() {
-        mDisposable.add(Observable.<Boolean>create(emitter -> {
-                    Request request = new Request.Builder()
-                            .url("https://api.vk.com/method/friends.get?fields=photo_100&v=5.95&access_token="
-                                    + mUser.mToken)
-                            .build();
-                    try (Response response = client.newCall(request).execute()) {
-                        Log.d(TAG, response.body().toString());
-                        mFriends.setFriends(response.body().toString());
-                        emitter.onNext(true);
-                    }
-                    emitter.onComplete();
-                })
-                        .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe(mUI::friendsDownloaded,
-                                e -> mUI.friendsDownloaded(false))
-        );
+    public Observable<ArrayList<Friend>> downloadFriends() {
+        return Observable.<ArrayList<Friend>>create(emitter -> {
+            Request request = new Request.Builder()
+                    .url("https://api.vk.com/method/friends.get?fields=photo_100, status&v=5.95&access_token="
+                            + mUser.mToken)
+                    .build();
+            try (Response response = client.newCall(request).execute()) {
+                if (response.isSuccessful())
+                    mFriends.setFriends(response.body().string());
+            }
+            emitter.onNext(mFriends.mFriends);
+            emitter.onComplete();
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread()
+                );
     }
 
 
@@ -100,14 +93,15 @@ public class DataRepository implements AuthCallback {
      * Dispose all subscription and clear data
      */
     public void dispose() {
-        mDisposable.dispose();
         mFriends.dispose();
         mPhotos.dispose();
     }
 
 
     /**
-     * Get data of user authentication process and pull result to UI through callback
+     * Get data of user authentication process and pull result
+     * to UI through callback
+     *
      * @param fragments the result string of authentication process
      */
     @Override
@@ -117,12 +111,35 @@ public class DataRepository implements AuthCallback {
 
         // If user cancel authentication
         if (data[0].contains("error")) {
-            mUI.userAuthenticated(false);
+            mUI.authenticateUser(false);
+            return;
         }
 
         // Initialize user data
         mUser = new User(data[0].replace("access_token=", ""),
                 Integer.parseInt(data[2].replace("user_id=", "")));
-        mUI.userAuthenticated(true);
+        mStorage.saveUserData(mUser); //save data to prefs
+        mUI.authenticateUser(true); //notify UI
+    }
+
+    /**
+     * Check is user authenticated and get its info if true
+     * This method must be called before any transactions
+     *
+     * @param preferences link to preferences
+     * @return is user authenticated
+     */
+    public boolean checkAuthentication(SharedPreferences preferences) {
+        mStorage.initialize(preferences);
+
+        if (mStorage.isUserAuthenticated()) {
+            try {
+                mUser = mStorage.getUserData();
+
+                return true;
+            } catch (Exception e) {
+                return false;
+            }
+        } else return false;
     }
 }
