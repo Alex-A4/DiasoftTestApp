d2da75e609eae85c6bcc1f41ca8cf44a59b9cce5
Alex <alex.A4.25@yandex.ru>
2019-05-28 00:11:12 +0300

    Add initializing of bloc

--- Flutter/friend_photo_flutter/lib/src/app.dart
+++ Flutter/friend_photo_flutter/lib/src/app.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:friend_photo_flutter/src/bloc/bloc.dart';
+import 'package:provider/provider.dart';
 
+import 'ui/main_screen.dart';
+
+/// Widget that initialize provider and starting screen
 class App extends StatelessWidget {
+  final Bloc bloc = Bloc();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Provider(
+      builder: (_) => bloc,
+      child: MaterialApp(
+        theme: theme,
+        home: MainScreen(),
+      ),
     );
   }
 }
+
+/// Default theme of app
+final theme = ThemeData(
+  primaryColor: Color(0xFFB0283C),
+  accentColor: Color(0xFFe75c66),
+);
