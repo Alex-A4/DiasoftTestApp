1a7e664bda44bc6497f94869befa4645ea0b52d9
Alex <alex.A4.25@yandex.ru>
2019-05-28 10:28:24 +0300

    Fill loading screen

--- Flutter/friend_photo_flutter/lib/src/ui/launch_screen.dart
+++ Flutter/friend_photo_flutter/lib/src/ui/launch_screen.dart
@@ -3,8 +3,32 @@ import 'package:flutter/material.dart';
 class LaunchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Expanded(flex: 1, child: Container()),
+          Expanded(
+            flex: 2,
+            child: FittedBox(
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/smile.png'),
+                  ),
+                  shape: BoxShape.circle,
+                ),
+                child: CircularProgressIndicator(strokeWidth: 1.0),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Text('Preparing to work'),
+          ),
+        ],
+      ),
     );
   }
 }
