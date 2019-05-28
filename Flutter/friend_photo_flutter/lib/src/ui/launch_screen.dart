63aeedcae297fe704517fcd293481e7f15d3becd
Alex <alex.A4.25@yandex.ru>
2019-05-28 13:58:49 +0300

    Fixed widgets displaying

--- Flutter/friend_photo_flutter/lib/src/ui/launch_screen.dart
+++ Flutter/friend_photo_flutter/lib/src/ui/launch_screen.dart
@@ -3,31 +3,38 @@ import 'package:flutter/material.dart';
 class LaunchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          Expanded(flex: 1, child: Container()),
-          Expanded(
-            flex: 2,
-            child: FittedBox(
-              child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/smile.png'),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: FittedBox(
+                child: Container(
+                  margin: const EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/logo.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                    shape: BoxShape.circle,
                   ),
-                  shape: BoxShape.circle,
+                  child: CircularProgressIndicator(strokeWidth: 1.0),
                 ),
-                child: CircularProgressIndicator(strokeWidth: 1.0),
               ),
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Text('Preparing to work'),
-          ),
-        ],
+            Expanded(
+              flex: 1,
+              child: Text(
+                'Preparing to work',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 25.0),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
